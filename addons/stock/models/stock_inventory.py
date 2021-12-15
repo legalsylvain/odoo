2e3da994ee58ec2fb2cdbd8391050bc66cdcdbf4
Sylvain LE GAL <sylvain.legal@grap.coop>
2021-12-15 11:44:51 +0100

    [FIX] stock : full inventory should also propose disabled product if quantity are not null

--- addons/stock/models/stock_inventory.py
+++ addons/stock/models/stock_inventory.py
@@ -236,7 +236,7 @@ class Inventory(models.Model):
     def _get_inventory_lines_values(self):
         # TDE CLEANME: is sql really necessary ? I don't think so
         locations = self.env['stock.location'].search([('id', 'child_of', [self.location_id.id])])
-        domain = ' sq.location_id in %s AND sq.quantity != 0 AND pp.active'
+        domain = ' sq.location_id in %s AND sq.quantity != 0'
         args = (tuple(locations.ids),)
 
         vals = []
